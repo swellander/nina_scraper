750fb9993057aa6f3337c494168890920a54fe56
Sam Wellander <samwellander@gmail.com>
2019-11-02 15:29:58 -0400

    additions

--- app.py
+++ app.py
@@ -1,45 +1,37 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
-from bs4 import BeautifulSoup
 import re
 import pandas as pd
 import os
 import time
 import requests
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
+from sms import send
+from dotenv import load_dotenv
+from loading_spinner import Spinner
+
+# Load environment variables
+load_dotenv()
 
 # Constants
-job_posting = 'New Grad'
-event_name = 'new_grad'
-sms_trigger_url = 'https://maker.ifttt.com/trigger/new_grad/with/key/biRPCs-8xjD_sq_NAlcN0dynwzvl1ALKIGB7kxX4F2A'
-posting_url = "https://childrensmn.taleo.net/careersection/chc_nursing/jobsearch.ftl?lang=en&radiusType=M&searchExpanded=true&radius=1&jobfield=200126570"
+target = os.getenv('TARGET')
+url = os.getenv('MONITORED_URL')
+list_selector = os.getenv('LIST_SELECTOR')
+item_selector = os.getenv('ITEM_SELECTOR')
+success_msg = os.getenv('SUCCESS_MSG', f'Success! Your target was found at {url}')
 
-def trigger_success_phone_call(url):
-    params = {"value1": url}
-    req = requests.post(url=sms_trigger_url, params=params)
 
-def check_for_position(job_posts=[], url=posting_url):
-    found = False
-    num_jobs = len(job_posts)
+def item_is_in_list(list_items=[]):
+    for item in list_items:
+        if target.lower() in item.text.lower():
+            return True
+    return False
 
-    for job_post in job_posts:
-        if job_posting.lower() in job_post.text.lower():
-            found = True
 
-    if found:
-        print(job_posting + ' job was found!')
-        trigger_success_phone_call(url)
-        return int(num_jobs / 3)
-
-    else:
-        return int(num_jobs / 3)
-
-def run_scrape():
-    print('Scraping Childrens Minnesota job listings page.')
-    print('====================================================')
-
-    chrome_bin = os.environ.get('GOOGLE_CHROME_SHIM', None)
-    executable_path = str(os.environ.get('CHROMEDRIVER_PATH'))
+def get_chrome_driver():
+    chrome_bin = os.getenv('GOOGLE_CHROME_SHIM')
+    executable_path = str(os.getenv('CHROMEDRIVER_PATH', '~/Downloads/chromedriver.exe'))
     chrome_options = Options()
     chrome_options.binary_location = chrome_bin
     chrome_options.add_argument("--headless")
@@ -47,33 +39,41 @@ def run_scrape():
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--remote-debugging-port=9222')
     driver = webdriver.Chrome(executable_path=executable_path, options=chrome_options)
+
+    return driver
+
+
+def run_scan():
+    print(f'Scanning for target: {target}')
+    print(f'at url: {url}')
+    print('====================================================')
+
+    driver = get_chrome_driver()
     driver.implicitly_wait(3000)
-    driver.get(posting_url)
+    driver.get(url)
 
     num_jobs = 0
 
-    # Wait for job postings to lazy load
-    time.sleep(2)
+    # Wait for page to load
+    print('Waiting for page to load')
+    with Spiner():
+        time.sleep(10)
+    print('Page loaded')
 
+    print('Scanning')
     first_page = BeautifulSoup(driver.page_source, features="html.parser")
-    job_posts = first_page.find_all('div', {"class": "absolute"})
 
-    next_button = driver.find_element_by_id('next')
-    next_button.click() 
+    # If searching for item within a list
+    if list_selector:
+        list_items = first_page.find_all(list_selector)
+        if item_is_in_list(list_items):
+            print('Item found!')
+            send(success_msg)
+            print(f'Text notification sent to {os.getenv("TO_NUMBER")}')
+        else:
+            print(f'Scanned {len(list_items)} items. Item not found :(')
+        
+    else:
+        raise Exception('Must supply a value for LIST_SELECTOR')
 
-    # Wait for job postings to lazy load
-    time.sleep(2)
-    jobs_scraped = check_for_position(job_posts=job_posts, url=driver.current_url)
-    num_jobs += jobs_scraped
-
-    second_page = BeautifulSoup(driver.page_source, features="html.parser")
-    job_posts = second_page.find_all('div', {"class": "absolute"})
-
-    jobs_scraped = check_for_position(job_posts=job_posts, url=driver.current_url)
-    num_jobs += jobs_scraped
-
-    print('Scraping ' + str(num_jobs) + ' nursing jobs')
-
-    print('Job not found :(')
-
-run_scrape()
+run_scan()
